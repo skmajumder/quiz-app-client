c79e1338ab190ffad00e51fa633abd8a62a92cc4
skmajumder <skmajumder.cse@gmail.com>
2023-09-23 16:52:19 +0600

    Main page style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import "./../styles/Main.css";
 
 const Main = () => {
-    return (
-        <div>
-            <h2>Main Root</h2>
-        </div>
-    );
+  const inputRef = useRef(null);
+
+  console.log(inputRef);
+
+  function startQuiz() {}
+
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application</h1>
+      <ol>
+        <li>You will be asked 10 questions one after another.</li>
+        <li>10 points is awarded for the correct answer.</li>
+        <li>
+          Each question has three options. You can choose only one options.
+        </li>
+        <li>You can review and change answers before the quiz finish.</li>
+        <li>The result will be declared at the end of the quiz.</li>
+      </ol>
+
+      <form id="form">
+        <input
+          ref={inputRef}
+          className="userid"
+          type="text"
+          placeholder="Input Your User Name"
+        />
+      </form>
+
+      <div className="start">
+        <Link className="btn" to={"/quiz"} onClick={startQuiz}>
+          Start Quiz
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
